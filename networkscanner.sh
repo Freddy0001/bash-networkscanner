7cd34e36c5a4aa15a02a85c1e2a13e9aaf58e66b
Freddy <fdrmatthews4@gmail.com>
2019-10-13 21:52:45 -0400

    Add comments to networkscanner.sh

--- networkscanner.sh
+++ networkscanner.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-host = $1
-startport = $2
-endport = $3
+# user input variables
+host = $1 # host server
+startport = $2 # starting port for port scan
+endport = $3 # ending port for port scan
 
 function pingcheck {
-	ping = 'ping -c 1 $host | grep bytes | wc -l'
-	if [ "$ping" -gt 1]: then
+	ping = 'ping -c 1 $host | grep bytes | wc -l' # ping host server
+	if [ "$ping" -gt 1]: then # if host is up
 		echo "$host is up";
-	else
+		portcheck # run portcheck funtion, if server host is up
+	else # if host is down
 		echo "$host is down, quitting"
-		exit
-	fi
+		exit # exit program
+	fi # end if statment
 }
 
 funtion portcheck {
-	for ((counter=$startport; counter<=$endport; counter++))
+	for ((counter=$startport; counter<=$endport; counter++)) # variable counter is made equal to startport, every time counter is less than the endport, counter is increased by one
 do
-	(echo >/dev/tcp/$host/$counter) > /dev/null 2>&1 && echo "$counter open"
+	(echo >/dev/tcp/$host/$counter) > /dev/null 2>&1 && echo "$counter open" # run tcp port check on counter variable
 done
 }
 
-pingcheck
-portcheck
+pingcheck # ping host server to check if its up, if it is, then run port check
